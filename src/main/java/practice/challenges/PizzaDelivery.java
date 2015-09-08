712202b04eff005f1bfaa20fa6d422e2e3ef1b7a
SANTOSH MANUGHALA <santu.0567@gmail.com>
2015-09-07 19:52:13 -0500

    Implemented pizza pblm

--- src/main/java/practice/challenges/PizzaDelivery.java
+++ src/main/java/practice/challenges/PizzaDelivery.java
@@ -1,9 +1,94 @@
 package practice.challenges;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
- * Created by SANTOSH on 9/6/2015.
+ * Created by SANTOSH on 9/7/2015.
  */
 public class PizzaDelivery {
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter the number of test cases: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
+        if(n < 1 || n > 20) {
+            throw new IllegalArgumentException("The number of test cases (n) must be greater than 1 and less than 20, but was " + n);
+        }
+
+        StringBuilder output = new StringBuilder();
+
+        for(int i = 1; i <= n; i++) {
+            System.out.println("Enter 2 integers x and y separated by space:");
+
+            String values = br.readLine();
+            String[] split = values.split(" ");
+            int x;
+            int y;
+
+            if(split.length != 2) {
+                throw new IllegalArgumentException("Expecting number of arguments to be 2, each separated with a space, but were: " + split.length + " arguments and the values being :" + values);
+            }
+
+            x = Integer.parseInt(split[0]);
+            if(x < 1 || x > 100) {
+                throw new IllegalArgumentException("The value of x  must be greater than 1 and less than 100, but was " + x);
+            }
+
+            y = Integer.parseInt(split[1]);
+            if(y < 1 || y > 1000) {
+                throw new IllegalArgumentException("The value of y must be greater than 1 and less than 100, but was " + y);
+            }
+
+
+            System.out.println("Enter " + x + " integers each line separated by a space in " + y + " lines:");
+
+            int[][] costMatrix = new int[x][y];
+
+            for(int p = 0; p <= y-1; p++) {
+                String rowValues = br.readLine();
+                String[] rowsSplit = rowValues.split(" ");
+                if(rowsSplit.length != x) {
+                    throw new IllegalArgumentException("Expecting number of arguments to be " + x + ", each separated with a space, but were: " + rowsSplit.length);
+                }
+
+                for(int q = 0; q <= x-1; q++) {
+                    costMatrix[q][p] = Integer.parseInt(rowsSplit[q]);
+                    if(costMatrix[q][p] < 0 || costMatrix[q][p] > 1000) {
+                        throw new IllegalArgumentException("The number of deliveries made to each street crossing must be greater than 0 and less than 1000, but was " +  costMatrix[q][p]);
+                    }
+                }
+            }
+
+            output.append(getOptimalDeliveryCost(costMatrix))
+                  .append(" blocks")
+                  .append("\n");
+        }
+
+        System.out.println(output);
+    }
+
+    public static int getOptimalDeliveryCost( int costMatrix[][]) {
+        int optimalDeliveryCost = 1000;
+
+        for(int p = 0; p < costMatrix.length; p++) {
+            for(int q = 0; q < costMatrix[0].length; q++) {
+
+                int totalDeliveryCost = 0;
+                for(int i = 0; i < costMatrix.length; i++) {
+                    for(int j = 0; j < costMatrix[0].length; j++) {
+                        totalDeliveryCost = totalDeliveryCost + (Math.abs(p- i) + Math.abs(q-j)) * costMatrix[i][j];
+                    }
+                }
+
+                if(optimalDeliveryCost > totalDeliveryCost) {
+                    optimalDeliveryCost = totalDeliveryCost;
+                }
+            }
+        }
+
+        return optimalDeliveryCost;
+    }
 }
--- src/main/java/practice/challenges/TomographyOperation.java
+++ src/main/java/practice/challenges/TomographyOperation.java
@@ -3,144 +3,108 @@ package practice.challenges;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Vector;
 
 
 /**
- * Created by SANTOSH on 9/6/2015.
+ * Created by SANTOSH on 9/7/2015.
  */
 public class TomographyOperation {
 
     public static void main(String[] args) throws IOException {
-        Vector<Integer> v1 = new Vector<>(3);
-        v1.add(0);
-        v1.add(0);
-        v1.add(3);
-//        v1.add(2);
-//        v1.add(3);
-//        v1.add(2);
-//        Vector<Integer> v2 = new Vector<>(4);
-        Vector<Integer> v2 = new Vector<>(3);
-        v2.add(0);
-        v2.add(0);
-        v2.add(3);
-//        v2.add(1);
-//        v2.add(1);
-//        v2.add(3);
-//        v2.add(2);
-        System.out.println("HERE1");
-        if(statisfiable(v1, v2)) {
-            System.out.println("YEs");
+        System.out.println("Enter 2 numbers separated by space:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String values = br.readLine();
+        String[] split = values.split(" ");
+        int m;
+        int n;
+
+        if(split.length != 2){
+            throw new IllegalArgumentException("Expecting number of arguments to be 2, each separated with a space, but were: " + split.length + " arguments and the values being: " + values);
+        }
+
+        m = Integer.parseInt(split[0]);
+        if(m < 1) {
+            throw new IllegalArgumentException("The value of m (number of rows) must not be less than 1, but was " + m);
+        }
+
+        n = Integer.parseInt(split[1]);
+        if(n < 1 || n > 1000) {
+            throw new IllegalArgumentException("The value of n (number of columns) must be greater than 1 and less than 1000, but was " + n);
+        }
+
+
+        String rowsInput;
+        System.out.println("Enter " + m + " numbers between 0 and " + n + " :");
+        rowsInput = br.readLine();
+
+        String[] rowsSplit = rowsInput.split(" ");
+        Vector<Integer> rows = new Vector<>(m);
+
+        if(rowsSplit.length != m) {
+            throw new IllegalArgumentException("Expecting number of arguments to be " + m + ", each separated with a space, but were: " + values);
+        }
+
+        for(int i = 0; i < m; i++) {
+            int temp = Integer.parseInt(rowsSplit[i]);
+            if( temp < 0 || temp > n) {
+                throw new IllegalArgumentException("The sum of each row in the matrix must be between 0 and " + n + ", but was " + temp);
+            }
+            rows.add(temp);
+        }
+
+
+        String columnsInput;
+        System.out.println("Enter " + n + " numbers between 0 and " + m + " :");
+        columnsInput = br.readLine();
+
+        String[] columnsSplit = columnsInput.split(" ");
+        Vector<Integer> columns = new Vector<>(n);
+
+        if(columnsSplit.length != n) {
+            throw new IllegalArgumentException("Expecting number of arguments to be " + n + ", each separated with a space, but were: " + values);
+        }
+
+        for(int i = 0; i < n; i++) {
+            int temp = Integer.parseInt(columnsSplit[i]);
+            if( temp < 0 || temp > n) {
+                throw new IllegalArgumentException("The sum of each row in the matrix must be between 0 and " + m + ", but was " + temp);
+            }
+            columns.add(Integer.parseInt(columnsSplit[i]));
+        }
+
+        if(isExistsTomography(rows, columns)) {
+            System.out.println("Yes");
             return;
         }
 
-        System.out.println("NO");
+        System.out.println("No");
         return;
-//        System.out.println("Enter to values: x y");
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//
-//        String values = br.readLine();
-//        String[] split = values.split(" ");
-//        int m;
-//        int n;
-//        if(split.length == 2)
-//        {
-//            m = Integer.parseInt(split[0]);
-//            if(m < 1) {
-//                throw new IllegalArgumentException("The value of m (number of rows) must not be less than 1");
-//            }
-//
-//            n = Integer.parseInt(split[1]);
-//            if(n < 1 || n > 1000) {
-//                throw new IllegalArgumentException("The value of n (number of columns) must be greater than 1 and less than 1000");
-//            }
-//        }else{
-//            throw new IllegalArgumentException("Expecting number of arguments to be 2, each separated with a space, but were: " + values);
-//        }
-//
-//        System.out.println("m = " + m);
-//        System.out.println("n = " + n);
-//
-//
-//        String rowsInput;
-//        System.out.println("Enter m numbers between 0 and " + n + " :");
-//        rowsInput = br.readLine();
-//        String[] rowsSplit = rowsInput.split(" ");
-//        List<Integer> rows = new ArrayList<Integer>();
-//
-//        if(rowsSplit.length == m)
-//        {
-//            for(int i = 0; i < m-1; i++) {
-//                int temp = Integer.parseInt(rowsSplit[i]);
-//                if( temp < 0 || temp > n) {
-//                    throw new IllegalArgumentException("The sum of each row in the matrix must be between 0 and " + n + ", but was " + temp);
-//                }
-//                rows.add(temp);
-//            }
-//        }else{
-//            throw new IllegalArgumentException("Expecting number of arguments to be " + m + ", each separated with a space, but were: " + values);
-//        }
-//
-//        String columnsInput;
-//        System.out.println("Enter n numbers between 0 and " + m + " :");
-//        columnsInput = br.readLine();
-//        String[] columnsSplit = columnsInput.split(" ");
-//        List<Integer> columns = new ArrayList<Integer>();
-//
-//        if(columnsSplit.length == n)
-//        {
-//            for(int i = 0; i < n-1; i++) {
-//                int temp = Integer.parseInt(columnsSplit[i]);
-//                if( temp < 0 || temp > n) {
-//                    throw new IllegalArgumentException("The sum of each row in the matrix must be between 0 and " + m + ", but was " + temp);
-//                }
-//                columns.add(Integer.parseInt(columnsSplit[i]));
-//            }
-//        }else{
-//            throw new IllegalArgumentException("Expecting number of arguments to be " + n + ", each separated with a space, but were: " + values);
-//        }
-//
-//        if(tomographyPossible(rows, columns)) {
-//            return "Yes";
-//        }
-
     }
 
-    private static boolean statisfiable(Vector<Integer> a, Vector<Integer> b) {
-        System.out.println("HERE2");
-        while(!a.isEmpty()) {
-            b.sort(Comparator.<Integer>reverseOrder());
-            int k = a.lastElement();
-            a.remove(a.indexOf(a.lastElement()));
-            if(k > b.size()) return false;
+    private static boolean isExistsTomography(Vector<Integer> rows, Vector<Integer> columns) {
+        while(!rows.isEmpty()) {
+            columns.sort(Comparator.<Integer>reverseOrder());
+            int k = rows.lastElement();
+            rows.remove(rows.indexOf(rows.lastElement()));
+            if(k > columns.size()) return false;
             if(k == 0) return false;
-            if(b.get(k - 1) == 0) return false;
+            if(columns.get(k - 1) == 0) return false;
             for(int i=0; i < k; i++) {
-                int j = b.get(i);
-                b.remove(i);
-                j--;
-                b.add(i, j);
+                int j = columns.get(i);
+                columns.remove(i);
+                columns.add(i, --j);
             }
         }
 
-        Iterator<Integer> i = b.iterator();
-        while(i.hasNext())
-            if(i.next() != 0)
+        Iterator<Integer> iterator = columns.iterator();
+        while(iterator.hasNext())
+            if(iterator.next() != 0)
                 return false;
 
         return true;
-    }
-
-    private static boolean tomographyPossible(List<Integer> rows, List<Integer> columns) {
-
-        for(int i = 1; i < rows.size() - 1; i++){
-
-            for(int j = 1; j < columns.size() - 1; j++) {
-
-            }
-        }
-
-
-        return false;
     }
 }
